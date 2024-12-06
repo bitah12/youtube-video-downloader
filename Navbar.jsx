d9537dd65cddae9744493e475a3ec2fad482f150
Rbitah <118397964+Rbitah@users.noreply.github.com>
2024-12-06 23:20:21 +0200

    Update Navbar.jsx

--- Navbar.jsx
+++ Navbar.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Navbar = () => {
   return (
     <nav className="bg-gray-800 text-white">
-    
+      <div className="container mx-auto flex items-center justify-between py-4 px-6">
+       
+      </div>
     </nav>
   );
 };
